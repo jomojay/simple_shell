02e7bc4f1b5d206e1edc2bf7dab34749187d9ecf
Precious <preciouskay4real@gmail.com>
2023-09-19 04:17:46 -0700

    builtins.c

---
+++ builtins.c
@@ -0,0 +1,88 @@
+#include "shell.h"
+
+/**
+ * dis_env - Display enviroment variable
+ * @cmd: Parsed command
+ * @st: previous execution status
+ * Return: Always 0
+ */
+int dis_env(__attribute__((unused)) char **cmd, __attribute__((unused)) int st)
+{
+	size_t i;
+	int len;
+
+	for (i = 0; environ[i] != NULL; i++)
+	{
+		len = _strlen(environ[i]);
+		write(1, environ[i], len);
+		write(STDOUT_FILENO, "\n", 1);
+	}
+	return (0);
+}
+/**
+ * exit_sh - Exit Statue Shell
+ * @cmd: Parsed Command
+ * @input: User Input
+ * @argv:Program Name
+ * @c:Excute Count
+ * Return: Void (Exit Statue)
+ */
+void  exit_sh(char **cmd, char *input, char **argv, int c)
+{
+	int status, i = 0;
+
+	if (cmd[1] == NULL)
+	{
+		free(input);
+		free(cmd);
+		exit(EXIT_SUCCESS);
+	}
+	while (cmd[1][i])
+	{
+		if (_isalpha(cmd[1][i++]) != 0)
+		{
+			_prerror(argv, c, cmd);
+			break;
+		}
+		else
+		{
+			status = _atoi(cmd[1]);
+			free(input);
+			free(cmd);
+			exit(status);
+		}
+	}
+}
+/**
+ * change_dir - Changes Dirctory
+ * @cmd: Parsed Command
+ * @status: previous execution status
+ * Return: 0 Succes or 1 Failed
+ */
+int change_dir(char **cmd, __attribute__((unused))int status)
+{
+	int value = -1;
+	char cwd[PATH_MAX];
+
+	if (cmd[1] == NULL)
+		value = chdir(getenv("HOME"));
+	else if (_strcmp(cmd[1], "-") == 0)
+	{
+		value = chdir(getenv("OLDPWD"));
+	}
+	else
+		value = chdir(cmd[1]);
+
+	if (value == -1)
+	{
+		perror("hsh");
+		return (-1);
+	}
+	else if (value != -1)
+	{
+		getcwd(cwd, sizeof(cwd));
+		setenv("OLDPWD", getenv("PWD"), 1);
+		setenv("PWD", cwd, 1);
+	}
+	return (0);
+}
