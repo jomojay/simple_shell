6b1943aecdbb73cfc772b47e2fc1acd0f58622d9
Precious <preciouskay4real@gmail.com>
2023-09-19 04:22:26 -0700

    error_printer.c

---
+++ error_printer.c
@@ -0,0 +1,43 @@
+#include "shell.h"
+
+/**
+ *  _prerror - Print Custome Error
+ * @argv:Program Name
+ * @c:Error Count
+ * @cmd:Command
+ * Return: Void
+ */
+void _prerror(char **argv, int c, char **cmd)
+{
+	char *er = _itoa(c);
+
+	PRINTER(argv[0]);
+	PRINTER(": ");
+	PRINTER(er);
+	PRINTER(": ");
+	PRINTER(cmd[0]);
+	PRINTER(": Illegal number: ");
+	PRINTER(cmd[1]);
+	PRINTER("\n");
+	free(er);
+}
+/**
+* print_error - Display Error Based on Command and How Many Time Shell Looped
+* @input:User Input
+* @counter:Simple Shell Count Loop
+* @argv:Program Name
+* Return: Void
+*/
+void print_error(char *input, int counter, char **argv)
+{
+	char *er;
+
+	PRINTER(argv[0]);
+	PRINTER(": ");
+	er = _itoa(counter);
+	PRINTER(er);
+	free(er);
+	PRINTER(": ");
+	PRINTER(input);
+	PRINTER(": not found\n");
+}
